6e184d04cfac63f13a6a2057bd8e72731dec12cc
George Donskoy <gdonskoy@indeed.com>
2019-04-28 18:28:44 -0700

    Setting up environment

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 import mongodb from 'mongodb';
 
 export default {
-  // "port": 3005,
-  // "mongoUrl": "mongodb://localhost:27017/chat-api",
-  "port": process.env.PORT,
-  "mongoUrl": process.env.MONGODB_URI,
+  //"port": 3005,
+  //"mongoUrl": "mongodb://localhost:27017/chat-api",
+   "port": process.env.PORT,
+   "mongoUrl": "mongodb://georgey:Mozkai2!@ds147073.mlab.com:47073/heroku_88hjscpb",
   "bodyLimit": "100kb"
 }
